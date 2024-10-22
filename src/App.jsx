1e8fef7a62f1ce1aa0d5d96c41a6235a4198da0d
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-10-23 02:25:07 +0530

    feat: mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import styles from "./app.module.scss";
 import ImagePreloader from "../components/ImagePreloader/ImagePreloader";
 
 function App() {
-  const imagePreloadArray = ["/desktopBackground.svg", "/phones.png"];
+  const imagePreloadArray = [
+    "/desktopBackground.svg",
+    "/phones.png",
+    "/apple.svg",
+    "/android.svg",
+  ];
   return (
     <>
       <ImagePreloader imageUrls={imagePreloadArray}>
         <div className={styles.container}>
           <div className={styles.content}>
+            <h1>ENTIRE FEST AT YOUR FINGERTIPS!</h1>
             <img
               src={imagePreloadArray[1]}
               alt="phones"
               className={styles.phones}
             />
+            <h2>DOWNLOAD THE APP NOW</h2>
+            <div className={styles.btnContainer}>
+              <button>
+                <img src={imagePreloadArray[2]} alt="apple download button" />
+              </button>
+              <button>
+                <img src={imagePreloadArray[3]} alt="android download button" />
+              </button>
+            </div>
           </div>
         </div>
         <div className={styles.backgroundGradient}></div>
